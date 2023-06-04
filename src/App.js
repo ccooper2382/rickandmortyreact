d3236b15187478d0fdd0ee11340bc7f1130e9d71
ccooper2382 <ccooper2382@gmail.com>
2023-06-04 12:26:20 -0500

    making slow progress on infinite scroll

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import CharacterList from "./components/characterList/characterList";
 
 function App() {
     const [characters, setCharacters] = useState([])
+    const [info, setInfo] = useState({})
     const [currentPage, setCurrentPage] = useState(1)
     //const [error, setError] = useState('')
 
     useEffect(() => {
         getData(currentPage)
-    }, []);
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, []); //don't listen to the warning here.  If there is no deps array it will keep loading data till it crashes
+
+
+   const updatePage = () => {
+
+       if(currentPage === 1) {
+            console.log("loading")
+        } else if  (currentPage <= info.pages) {
+            setCurrentPage(currentPage + 1)
+            console.log(info.pages + "RAWR")
+        } else {
+            setCurrentPage(info.pages)
+            console.log(info.pages + "blah")
+        }
+
+   }
+
+   const handleScroll = () => {
+       if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+       console.log('Fetch more list items!');
+   }
 
     const updateCharacters = (data) => {
         if (characters.length === 0) {
@@ -17,7 +40,6 @@ function App() {
         } else {
             setCharacters((prevCharacters) => [...prevCharacters, ...data]);
         }
-
     }
 
     const getData = (page) => {
@@ -32,6 +54,7 @@ function App() {
             })
             .then(data => {
                 updateCharacters(data.results);
+                setInfo(data.info)
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
