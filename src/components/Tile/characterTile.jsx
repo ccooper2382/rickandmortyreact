43f0b4db51d5df86deca8f670bbd128b60308745
ccooper2382 <ccooper2382@gmail.com>
2023-06-17 12:23:33 -0500

    Polishing and experimenting with nested css

--- src/components/Tile/characterTile.jsx
+++ src/components/Tile/characterTile.jsx
@@ -4,14 +4,16 @@ import Episodes from "../episodes/episodes";
 import Location from "../location/location";
 import styles from './tile.module.css';
 
-function CharacterTile({name, status, species, type, gender, image, location, origin, episodes, info}) {
+function CharacterTile({name, status, species, type, gender, image, location, origin, episodes}) {
 
     return (
         <article className={styles.flexContainer}>
             <Title name={name} species={species} status={status} image={image} type={type} gender={gender}
                    origin={origin} location={location}/>
+            <section className={styles.info}>
             <Location location={location} origin={origin}/>
             <Episodes episodes={episodes}/>
+            </section>
         </article>
 
     );
--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -5,12 +5,12 @@ import TitleStats from "./TitleStats";
 
 function Title({image, name, status, species, type, gender,}) {
     return (
-        <div className={styles.character__Title}>
+        <section className={styles.character__Title}>
             <div className={styles.character__portrait}>
                 <img className={styles.character__image} src={image} alt={name}></img>
             </div>
             <TitleStats name={name} species={species} status={status} type={type} gender={gender}/>
-        </div>
+        </section>
     );
 }
 
