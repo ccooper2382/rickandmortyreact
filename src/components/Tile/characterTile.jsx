b4de08545d351aedf6fefe56b21ffcf1eb7d59b2
ccooper2382 <ccooper2382@gmail.com>
2023-06-17 12:25:31 -0500

    Polishing and experimenting with nested css

--- src/components/Tile/characterTile.jsx
+++ src/components/Tile/characterTile.jsx
@@ -10,10 +10,9 @@ function CharacterTile({name, status, species, type, gender, image, location, or
         <article className={styles.flexContainer}>
             <Title name={name} species={species} status={status} image={image} type={type} gender={gender}
                    origin={origin} location={location}/>
-            <section className={styles.info}>
             <Location location={location} origin={origin}/>
             <Episodes episodes={episodes}/>
-            </section>
+
         </article>
 
     );
